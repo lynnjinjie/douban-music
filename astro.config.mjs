59e79f710282925842baaa8d658451a5aee96f92
lynnjiang <lynnjinjie@gmail.com>
2024-12-12 22:36:54 +0800

    chore: add shadcn ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,12 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
+	integrations: [
+		tailwind({
+			applyBaseStyles: false
+		}),
+		react()
+	],
 	image: {
 		service: sharpImageService()
 	}
