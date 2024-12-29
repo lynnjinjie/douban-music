4317db2b7faf5ffc7e669afebfa19d6be56d0723
lynnjiang <lynnjinjie@gmail.com>
2024-12-29 23:35:03 +0800

    refactor(player): update PlayButton and TrackList components to use songList prop

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -1,19 +1,50 @@
 import { Button } from '@/components/ui/button'
 import { $playTrack, type PlayTrack } from '@/store'
 import { $playList } from '@/store'
+import { useStore } from '@nanostores/react'
+import { ListPlus, Play } from 'lucide-react'
 interface Props {
-	playList: PlayTrack[]
+	songList: PlayTrack[]
 }
 
-export default function PlayButton({ playList }: Props) {
+export default function PlayButton({ songList }: Props) {
+	const playList = useStore($playList)
+
 	const handlePlayAll = () => {
-		$playList.set(playList)
-		$playTrack.set(playList[0])
+		$playList.set(songList)
+		$playTrack.set(songList[0])
+	}
+
+	const disabledAddToPlayList = songList.every((song) =>
+		playList.some((item) => item.id === song.id)
+	)
+
+	const handleAddToPlayList = () => {
+		// 如果 playList 中已经存在 songList 中的歌曲，则不添加
+		const newPlayList = songList.filter((song) => !playList.some((item) => item.id === song.id))
+		$playList.set([...newPlayList, ...playList])
+		$playTrack.set(songList[0])
 	}
 
 	return (
-		<Button variant="outline" className="px-8 py-6 text-lg text-orange-500" onClick={handlePlayAll}>
-			播放全部
-		</Button>
+		<div className="flex items-center gap-2">
+			<Button
+				variant="outline"
+				className="px-8 py-6 text-lg text-orange-500"
+				onClick={handlePlayAll}
+			>
+				<Play className="mr-2 size-10" />
+				播放全部
+			</Button>
+			<Button
+				variant="secondary"
+				className="px-8 py-6 text-lg text-orange-500"
+				onClick={handleAddToPlayList}
+				disabled={disabledAddToPlayList}
+			>
+				<ListPlus className="mr-2 size-10" />
+				播放列表
+			</Button>
+		</div>
 	)
 }
--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -5,20 +5,21 @@ import { useEffect, useState } from 'react'
 import { type PlayTrack } from '@/store'
 
 type Props = {
-	playList: PlayTrack[]
+	songList: PlayTrack[]
+	id: string
 	index: number
 }
 
-export default function TrackList({ playList, index }: Props) {
+export default function TrackList({ songList, id, index }: Props) {
 	const [isPlaying, setIsPlaying] = useState(false)
 	const [isCurrentSongPlaying, setIsCurrentSongPlaying] = useState(false)
 
 	const { songName: currentSongName } = useStore($playTrack)
 	const isSongPlaying = useStore($isPlaying)
+	const playList = useStore($playList)
+	const currentSong = songList[index]
 
-	const currentSong = playList[index - 1]
-
-	const isHaveMp3 = !!currentSong.mp3Url
+	const isHaveMp3 = !!currentSong?.mp3Url
 
 	const isActiveColor = isCurrentSongPlaying ? 'text-orange-500' : ''
 
@@ -27,22 +28,33 @@ export default function TrackList({ playList, index }: Props) {
 		: 'cursor-not-allowed text-gray-300 dark:text-gray-500'
 
 	useEffect(() => {
-		setIsPlaying(isSongPlaying && currentSongName === currentSong.songName)
-		setIsCurrentSongPlaying(currentSongName === currentSong.songName)
-	}, [isSongPlaying, currentSongName, currentSong.songName])
+		setIsPlaying(isSongPlaying && currentSongName === currentSong?.songName)
+		setIsCurrentSongPlaying(currentSongName === currentSong?.songName)
+	}, [isSongPlaying, currentSongName, currentSong?.songName])
 
 	const handlePlay = () => {
 		if (isHaveMp3) {
-			$playList.set(playList)
+			// 如果当前歌曲不存在，将当前歌曲添加到播放列表的顶部
+			const isCurrentSongInPlayList = playList.some((item) => item.id === currentSong.id)
+			if (!isCurrentSongInPlayList) {
+				const newPlayList = [currentSong, ...playList]
+				$playList.set(newPlayList)
+			}
 			$playTrack.set(currentSong)
 		}
 	}
 
 	return (
-		<div className={`flex items-center border-b py-2 ${disabledClass}`} onClick={handlePlay}>
-			<span className={`mr-2 text-lg ${isActiveColor}`}>{index}.</span>
-			{isPlaying && <MusicIcon className={`size-4 ${isActiveColor}`} />}
-			<span className={`ml-2 text-lg ${isActiveColor}`}>{currentSong.songName}</span>
+		<div
+			className={`flex items-center justify-between border-b py-2 ${disabledClass}`}
+			onClick={handlePlay}
+		>
+			<div className="flex items-center">
+				<span className={`mr-2 text-lg ${isActiveColor}`}>{index + 1}.</span>
+				{isPlaying && <MusicIcon className={`size-4 ${isActiveColor}`} />}
+				<span className={`ml-2 text-lg ${isActiveColor}`}>{currentSong?.songName}</span>
+			</div>
+			<span className={`ml-2 text-lg ${isActiveColor}`}>{currentSong?.duration}</span>
 		</div>
 	)
 }
