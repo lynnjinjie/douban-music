7e2e01180efdc95be38fd3402981ad7e319bdc86
lynnjiang <lynnjinjie@gmail.com>
2025-01-01 21:10:47 +0800

    feat(TrackList): enhance track interaction with improved play/pause functionality, dynamic playlist updates, and refined UI elements

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -1,8 +1,9 @@
-import { MusicIcon } from '@/components/Icons'
 import { $playTrack, $isPlaying, $playList } from '@/store'
 import { useStore } from '@nanostores/react'
-import { useEffect, useState } from 'react'
 import { type PlayTrack } from '@/store'
+import { AudioLines, Check, Plus } from 'lucide-react'
+import { cn } from '@/lib/utils'
+import { FillPauseIcon, FillPlayIcon } from '@/components/Icons'
 
 type Props = {
 	songList: PlayTrack[]
@@ -11,52 +12,94 @@ type Props = {
 }
 
 export default function TrackList({ songList, id, index }: Props) {
-	const [isPlaying, setIsPlaying] = useState(false)
-	const [isCurrentSongPlaying, setIsCurrentSongPlaying] = useState(false)
-
-	const { songName: currentSongName } = useStore($playTrack)
-	const isSongPlaying = useStore($isPlaying)
+	const { id: playTrackId } = useStore($playTrack)
+	const isPlaying = useStore($isPlaying)
 	const playList = useStore($playList)
 	const currentSong = songList[index]
 
 	const isHaveMp3 = !!currentSong?.mp3Url
 
-	const isActiveColor = isCurrentSongPlaying ? 'text-orange-500' : ''
+	const isCurrentSongTrack = playTrackId === id
+
+	const isActiveColor = isCurrentSongTrack && 'text-orange-500'
+	const isCurrentSongInPlayList = playList.some((item) => item.id === currentSong.id)
 
 	const disabledClass = isHaveMp3
 		? 'cursor-pointer text-gray-500 dark:text-gray-300'
 		: 'cursor-not-allowed text-gray-300 dark:text-gray-500'
 
-	useEffect(() => {
-		setIsPlaying(isSongPlaying && currentSongName === currentSong?.songName)
-		setIsCurrentSongPlaying(currentSongName === currentSong?.songName)
-	}, [isSongPlaying, currentSongName, currentSong?.songName])
-
 	const handlePlay = () => {
 		if (isHaveMp3) {
+			// 如果当前歌曲是正在播放的歌曲，则暂停
+			if (isCurrentSongTrack) {
+				$isPlaying.set(!isPlaying)
+				return
+			}
 			// 如果当前歌曲不存在，将当前歌曲添加到播放列表的顶部
-			const isCurrentSongInPlayList = playList.some((item) => item.id === currentSong.id)
 			if (!isCurrentSongInPlayList) {
 				const newPlayList = [currentSong, ...playList]
 				$playList.set(newPlayList)
+				$playTrack.set(currentSong)
+				$isPlaying.set(true)
+				return
 			}
 			$playTrack.set(currentSong)
+			$isPlaying.set(true)
 		}
+	}
+
+	const handleAddToPlayList = () => {
+		const newPlayList = [...playList, currentSong]
+		$playList.set(newPlayList)
 	}
 
 	return (
 		<div
-			className={`flex items-center justify-between border-b py-2 ${disabledClass}`}
-			onClick={handlePlay}
+			className={cn(
+				'group flex items-center justify-between px-2 py-2 transition-opacity hover:bg-orange-500/10 hover:duration-1000',
+				isCurrentSongTrack && 'bg-orange-500/10',
+				disabledClass
+			)}
 		>
 			<div className="flex items-center">
-				<span className={`mr-2 ${isActiveColor}`}>{index + 1}.</span>
-				{isPlaying && <MusicIcon className={`size-4 ${isActiveColor}`} />}
+				<div
+					className={`relative inline-flex size-10 items-center justify-center ${isActiveColor}`}
+					onClick={handlePlay}
+				>
+					{isPlaying && isCurrentSongTrack ? (
+						<AudioLines className={`size-6 group-hover:opacity-0 ${isActiveColor}`} />
+					) : (
+						<span className="group-hover:opacity-0">{index + 1}</span>
+					)}
+					<span
+						className={cn(
+							'absolute inset-0 flex items-center justify-center rounded-md opacity-0',
+							'group-hover:opacity-100'
+						)}
+					>
+						{isPlaying && isCurrentSongTrack ? (
+							<FillPauseIcon className={cn('size-6', isActiveColor)} />
+						) : (
+							<FillPlayIcon className={cn('size-6', isActiveColor)} />
+						)}
+					</span>
+				</div>
 				<span className={`ml-2 line-clamp-1 text-base md:text-lg ${isActiveColor}`}>
 					{currentSong?.songName}
 				</span>
 			</div>
-			<span className={`ml-2 text-lg ${isActiveColor}`}>{currentSong?.duration}</span>
+			<span
+				className={`relative inline-flex w-12 items-center justify-center text-lg ${isActiveColor}`}
+			>
+				<span className="group-hover:opacity-0">{currentSong?.duration}</span>
+				<span className="absolute right-2 flex items-center gap-1 opacity-0 group-hover:opacity-100">
+					{isCurrentSongInPlayList ? (
+						<Check className="size-6" />
+					) : (
+						<Plus className="size-6" onClick={handleAddToPlayList} />
+					)}
+				</span>
+			</span>
 		</div>
 	)
 }
